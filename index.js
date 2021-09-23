72e944bc67e709ddc5fa98631e05bb97f113e1e3
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-23 14:51:48 -0400

    adds constructor

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const circle = {
    }
 };
 
-// if an object has "behavior" it shouldn't be an object literal. Then you need a factor or constructor function
+// if an object has "behavior" it shouldn't be an object literal. Then you need a Factory or Constructor function
 
 // factory function
 function createCircle(radius){
@@ -22,3 +22,14 @@ function createCircle(radius){
    }
   }
 }
+const newCircle = createCircle(1)
+
+// constructor function then it needs an upper case 
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function() {
+    console.log("draw")
+  }
+}
+
+const anotherCircle = new Circle(1);
