f697cbc518cfc64bd0a5cb9de9fc921168d9f4f3
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-23 14:40:24 -0400

    refactors object literal syntax to be a factory function

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ const circle = {
    }
 };
 
+// if an object has "behavior" it shouldn't be an object literal. Then you need a factor or constructor function
+
+// factory function
+function createCircle(radius){
+  return {
+    radius,
+    draw: function() {
+     console.log('draw');
+   }
+  }
+}
