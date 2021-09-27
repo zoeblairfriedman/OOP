3fa96ee420ea8e2fcece09b9b46c9ab86ddef989
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-09-27 11:48:36 -0400

    adding this

--- index3.js
+++ index3.js
@@ -1,33 +1,24 @@
+
+const Circle = function(){
+  this.draw = function() {
+    console.log(this)
+  }
+}
+
+const c = new Circle()
+c.draw();
+const draw = c.draw;
+
+//this will return the WINDOW!! cause it's not called on an instance of cirlce, it's being called on the window. if you added 'use strict' to the top at least it would return undefined. 
+draw()
+
+// however in ES6 anything in a class uses strict by default
 class Circle {
-  constructor(radius){
-    this.radius = radius;
-  }
-  //instance method <-- particular to a certain object
-  draw() {
-    console.log('draw')
-  }
-
-  // static method <-- not available to instances, only the class. Class method. 
-  static parse(str) {
-
+  draw(){
+    console.log(this)
   }
 }
 
-const c = new Circle(2);
-
-//if you put the method in the constructor it's a function on the object instance, if you put it in the body, it's on the prototype
-
-
-
-//declaration <--- these are hoisted to the top! 
-function sayHello(){
-
-}
-
-//expressions are not hoisted.
-const sayGoodbye = function(){
-
-};
-
-// this works too. but it won't be hoisted. Don't use this. 
-const Square = class {};
+const c = new Circle()
+const draw = c.draw;
+draw();
